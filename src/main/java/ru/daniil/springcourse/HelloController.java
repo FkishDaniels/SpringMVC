b3aea9df94f0d7bbec8905ce5504b6521fdfc3fa
FkishDaniels <irina2808777@gmail.com>
2023-03-13 23:49:50 +0300

    Java classes instead xml files!

--- src/main/java/ru/daniil/springcourse/HelloController.java
+++ src/main/java/ru/daniil/springcourse/HelloController.java
@@ -1,2 +1,13 @@
-package ru.daniil.springcourse;public class HelloController {
+package ru.daniil.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "hello_world";
+    }
 }
